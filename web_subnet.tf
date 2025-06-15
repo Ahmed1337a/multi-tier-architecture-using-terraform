b630ed59a53647ed3a8786cc548f171fd6c77550
Ahmed1337a <ahmed.kadry20211@gmail.com>
2025-06-15 22:33:42 +0300

    Create web_subnet.tf

---
+++ web_subnet.tf
@@ -0,0 +1,10 @@
+resource "aws_subnet" "public_subnets" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
