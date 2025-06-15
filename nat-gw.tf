1bfbb9d6304a447289c7a37680ea883fa22cb7cb
Ahmed1337a <ahmed.kadry20211@gmail.com>
2025-06-15 22:35:55 +0300

    Create nat-gw.tf

---
+++ nat-gw.tf
@@ -0,0 +1,11 @@
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+  tags = {
+    Name = "multi-tier-nat"
+  }
+}
